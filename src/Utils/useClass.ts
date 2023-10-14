667bff41c5d2d387ab6ef75772472acd1b7fec35
ruizavi <avi.ruiz@proton.me>
2023-10-14 15:43:47 -0600

    [FIX] no generate classes

--- src/Utils/useClass.ts
+++ src/Utils/useClass.ts
@@ -1,21 +1,22 @@
+import { useCallback } from 'react';
 import classNames from 'classnames';
 import { CLASS_PREFIX } from './const';
-import { useCallback } from 'react';
 
-function addPrefix(prefix: string, className: string | string[]) {
-   if (!prefix || className) return '';
+export function addPrefix(pre: string, className: string | string[]): string {
+   if (!pre || !className) {
+      return '';
+   }
 
-   if (Array.isArray(className))
-      return classNames(className.filter(n => !!n).map(n => `${prefix}-${n}`));
+   if (Array.isArray(className)) {
+      return classNames(className.filter(name => !!name).map(name => `${pre}-${name}`));
+   }
 
-   return `${prefix}-${className}`;
+   if (pre[pre.length - 1] === '-') {
+      return `${pre}${className}`;
+   }
+
+   return `${pre}-${className}`;
 }
-
-export interface ClassDictionary {
-   [id: string]: any;
-}
-
-export interface ClassArray extends Array<ClassValue> {}
 
 export type ClassValue =
    | string
@@ -26,29 +27,42 @@ export type ClassValue =
    | null
    | boolean;
 
+export interface ClassArray extends Array<ClassValue> {}
+
+export interface ClassDictionary {
+   [id: string]: any;
+}
+
 function useClass(str: string) {
    const classPrefix = CLASS_PREFIX;
    const componentClass = addPrefix(classPrefix, str);
 
    const prefix = useCallback(
       (...classes: ClassValue[]) => {
-         const mergeClasses = !classes.length
-            ? []
-            : classNames(...classes)
+         const mergeClasses = classes.length
+            ? classNames(...classes)
                  .split(' ')
-                 .map(item => addPrefix(componentClass, item));
+                 .map(item => addPrefix(componentClass, item))
+            : [];
 
          return mergeClasses.filter(cls => cls).join(' ');
       },
       [componentClass]
    );
 
-   const addClassPrefix = useCallback((...classes: ClassValue[]) => {
-      const mergeClasses = prefix(classes);
-      return mergeClasses ? `${componentClass} ${mergeClasses}` : componentClass;
-   }, []);
+   const addClassPrefix = useCallback(
+      (...classes: ClassValue[]) => {
+         const mergeClasses = prefix(classes);
+         return mergeClasses ? `${componentClass} ${mergeClasses}` : componentClass;
+      },
+      [componentClass, prefix]
+   );
 
-   return { merge: classNames, addClassPrefix };
+   return {
+      addClassPrefix,
+      merge: classNames,
+      prefix
+   };
 }
 
 export default useClass;
