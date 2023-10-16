5fae78d51dffc044bece115ad76b79b3dd635c13
ruizavi <avi.ruiz@proton.me>
2023-10-16 15:41:45 -0600

    [FIX] finally, package upload correctly to npm and work

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
+import { resolve } from 'node:path';
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react-swc';
+import * as packageJson from './package.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
    plugins: [react()],
    build: {
       lib: {
-         entry: 'src/index.tsx',
-         name: 'cold-plasma'
+         entry: resolve('src', 'index.tsx'),
+         name: 'cold-plasma',
+         formats: ['es', 'umd']
+      },
+      rollupOptions: {
+         external: [...Object.keys(packageJson.peerDependencies)]
       }
    },
    test: {
