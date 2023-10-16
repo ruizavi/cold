b4378a893fa8daf118cc014e999b87bdc662a938
ruizavi <avi.ruiz@proton.me>
2023-10-16 16:22:40 -0600

    [ADD] dts plugin for generate types

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,17 @@ import { resolve } from 'node:path';
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react-swc';
 import * as packageJson from './package.json';
-
+import dts from 'vite-plugin-dts';
 // https://vitejs.dev/config/
 export default defineConfig({
-   plugins: [react()],
+   plugins: [react(), dts({ rollupTypes: true })],
    build: {
       lib: {
          entry: resolve('src', 'index.tsx'),
          name: 'cold-plasma',
          formats: ['es', 'umd']
       },
+
       rollupOptions: {
          external: [...Object.keys(packageJson.peerDependencies)]
       }
