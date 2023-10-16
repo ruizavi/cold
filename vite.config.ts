b0d3c0f792a1cf1d69690ae32a7d38f879a2ba0d
ruizavi <avi.ruiz@proton.me>
2023-10-16 14:23:14 -0600

    [ADD] prepare to publish in npm

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
    plugins: [react()],
+   build: {
+      lib: {
+         entry: 'src/index.ts',
+         name: 'cold'
+      }
+   },
    test: {
       environment: 'happy-dom'
    }
